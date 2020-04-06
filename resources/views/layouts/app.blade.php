0af205d904b8574027686d9cf06d2be49771eaa0
DenisTSenov <denis_cenov_@abv.bg>
2020-04-06 16:08:50 +0300

    include js/css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,13 +1,20 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+    <!-- JS -->
+    <script type="text/javascript" src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}"/>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Training Manager - @yield('title')</title>
 </head>
 <body>
-    <div class="container">
-        @yield('content')
+    <div id="app">
+        <div class="container">
+            @yield('content')
+        </div>
     </div>
 </body>
 </html>

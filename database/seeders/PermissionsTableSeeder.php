eb921ae04cb9c7ee9ceb5b4596aecf22f413d862
vagrant <vagrant@homestead>
2021-04-05 09:04:07 +0000

    update laravel to 8.35

--- database/seeders/PermissionsTableSeeder.php
+++ database/seeders/PermissionsTableSeeder.php
@@ -16,7 +16,6 @@ class PermissionsTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
         DB::table('permissions')->truncate();
 
         DB::table('permissions')->insert([
--- database/seeders/RolesTableSeeder.php
+++ database/seeders/RolesTableSeeder.php
@@ -18,17 +18,13 @@ class RolesTableSeeder extends Seeder
     {
         DB::table('roles')->truncate();
         DB::table('permission_role')->truncate();
-        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 
         DB::table('roles')->insert([
             ['name' => 'Trainer', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
             ['name' => 'Competitor', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
         ]);
 
-        $trainer = Role::where('id', 1)->first();
-        $trainer->permissions()->attach([1, 2]);
-
-        $competitor = Role::where('id', 2)->first();
-        $competitor->permissions()->attach([5]);
+        Role::where('id', 1)->first()->permissions()->attach([1, 2]);
+        Role::where('id', 2)->first()->permissions()->attach([5]);
     }
 }
